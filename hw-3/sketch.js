80d4bdaaf047b0b9ea9cd856c011f06f33e2bba0
Haylie Peacock <haylie.peacock17@gmail.com>
2018-09-12 22:37:22 -0600

    Created Hello World

---
+++ hw-3/sketch.js
@@ -0,0 +1,9 @@
+//Creating canvas with pink background.
+function setup() {
+  createCanvas( 600, 400);
+  background( 'pink');
+}
+// Adding "Hello World" text, positioned at (x: 20, y: 30).
+function draw() {
+  text( "Hello World!", 20, 30 );
+}
