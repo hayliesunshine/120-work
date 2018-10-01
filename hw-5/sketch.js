41a631b8faa6b248b74b82576d64f99b55f7e51e
Haylie Peacock <haylie.peacock17@gmail.com>
2018-10-01 15:51:24 -0600

    debugging

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,18 +1,23 @@
 //Flying Bird
 
-let headAngle = 0;
-let headRotationRate = 60;
-let headWidth = 80;
-let headHeight = 80;
-let yPosition = mouseY;
-let xPosition = 0.25
+var headAngle;
+var headRotationRate;
+var headWidth;
+var headHeight;
+var yPosition;
+var xPosition;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
+headAngle = 0;
+headRotationRate = 60;
+headWidth = 80;
+headHeight = 80;
+xPosition = 0.25;
 
 function draw() {
+  yPosition = mouseY;
   //erase every frame
   background( 'rgb( 135, 206, 250)');
   //turn the cursor off
@@ -26,64 +31,61 @@ function draw() {
 
   push();
 
-  //make the bird move right and on mouseY
-  translate( xPosition + 1, yPosition);
+    //make the bird move right and on mouseY
+    xPosition = xPosition + 1;
+    translate( xPosition, yPosition);
 
-  //body
-  fill ( 'rgb(169, 169, 169)' );
-  noStroke();
-  //end body
-  pop();
+    push();
+      //body
+      fill ( 'rgb(169, 169, 169)' );
+      noStroke();
+      //end body
 
-  push();
-  //head
-  fill ( 'rgb(169, 169, 169)');
-  noStroke();
-  //rotate head based on headAngle
-  rotate( radians(headAngle) );
-  //draw head
-  ellipse( 0, 0, headWidth, headHeight );
-  noStroke();
+      //head
+      fill ( 'rgb(169, 169, 169)');
+      noStroke();
+      //rotate head based on headAngle
+      rotate( radians(headRotationRate) );
+      //draw head
+      ellipse( 0, 0, headWidth, headHeight );
+      noStroke();
+    pop();
 
-  //eyes
-  strokeWeight(2);
-  push();
-  //draw eyes based on head size
-  translate( headWidth * -0.2, headHeight * -0.2);
-  ellipse( 0, 0, headWidth * 0.33, headHeight * 0.33);
-  noStroke();
-  fill( 'rgb(255, 255, 255)');
-  ellipse( 0, 0, 5);
-  ellipse ( 0, 0, 10);
-  pop();
-  push();
-  translate( headWidth * 0.2, headHeight * -0.2);
-  ellipse( 0, 0, 20 );
-  noStroke();
-  fill( 'rgb(0)');
-  ellipse( 0, 0, 10);
-  ellipse( 0, 0, 5);
-  pop();
+    //eyes
+    strokeWeight(2);
+    push();
+      //draw eyes based on head size
+      translate( headWidth * -0.2, headHeight * -0.2);
+      ellipse( 0, 0, headWidth * 0.33, headHeight * 0.33);
+      noStroke();
+      fill( 'rgb(255, 255, 255)');
+      ellipse( 0, 0, 5);
+      ellipse ( 0, 0, 10);
+    pop();
 
-  //beak
-  push();
-  fill( 'rgb( 200, 80, 0)' );
-  triangle (40, 20, 80, 0, 40, -20 );
-  pop();
-  //end head
+    push();
+      translate( headWidth * 0.2, headHeight * -0.2);
+      ellipse( 0, 0, 20 );
+      noStroke();
+      fill( 'rgb(0)');
+      ellipse( 0, 0, 10);
+      ellipse( 0, 0, 5);
+    pop();
 
-  // wing
-  push();
-  fill( 'rgb(110, 110, 110)' );
-  stroke(2);
-  triangle( )
+    //beak
+      push();
+      fill( 'rgb( 200, 80, 0)' );
+      triangle (40, 20, 80, 0, 40, -20 );
+    pop();
+    //end head
 
+    // wing
+    push();
+      fill( 'rgb(110, 110, 110)' );
+      stroke(2);
+    pop();
 
-
-
-
-
-
+pop();
 
 
 }
